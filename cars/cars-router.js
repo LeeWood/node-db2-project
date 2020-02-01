65860805232764cb6cfb27d2407ed29fa7805b1d
lee-wood <leemaewood@gmail.com>
2020-01-31 23:20:20 -0500

    added GET and POST queries/endpoints MVP

---
+++ cars/cars-router.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const db = require('../data/db-config.js');
+
+const router = express.Router();
+
+//GET
+
+router.get('/', (req, res) => {
+
+  db('cars')
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Failed to retrieve cars.",
+        error: err
+      });
+    });
+});
+
+router.get('/:id', (req, res) => { //GET indivudual
+
+  const { id } = req.params;
+
+  db('fruits').where({ id }).first()
+    .then(car => {
+      res.status(200).json(car);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Failed to retrieve car",
+        error: err
+      });
+    });
+});
+
+//POST
+router.post('/', (req, res) => {
+
+  const carData = req.body;
+
+  db('cars').insert(carData)
+    .then(ids => {
+      db('cars').where({ id: ids[0] })
+        .then(newCarEntry => {
+          res.status(201).json(newCarEntry);
+        });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Failed to add data",
+        error: err
+      });
+    });
+});
+
+module.exports = router;
