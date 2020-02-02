dc866ed7f7109fb2ff045786982cc1b72f287776
lee-wood <leemaewood@gmail.com>
2020-02-01 21:05:24 -0500

    added update query

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -3,7 +3,7 @@ const db = require('../data/db-config.js');
 
 const router = express.Router();
 
-//GET
+// * GET
 
 router.get('/', (req, res) => {
 
@@ -35,7 +35,8 @@ router.get('/:id', (req, res) => { //GET indivudual
     });
 });
 
-//POST
+// * POST
+
 router.post('/', (req, res) => {
 
   const carData = req.body;
@@ -54,5 +55,28 @@ router.post('/', (req, res) => {
       });
     });
 });
+
+// * PUT
+
+router.put('/:id', (req, res) => {
+
+  const { id } = req.params;
+  const updatedInfo = req.body;
+
+  db('cars').where('id', id).update(updatedInfo)
+    .then(updatedCar => {
+      res.status(200).json({
+        updated: updatedCar
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "Failed to update car data.",
+          error: err
+        });
+      });
+    });
+});
+
+// * DELETE
 
 module.exports = router;
